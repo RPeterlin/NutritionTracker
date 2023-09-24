856794ddd38955ccfd00f49150cd351a87332e2b
RPeterlin <rok.peterlin.dev@gmail.com>
2023-09-24 11:26:53 +0200

    Fixed UI for dashboard, added the option to change base unit

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -18,10 +18,6 @@ export async function action(request, deleteElement, updateElement){
       // TODO
       break;
 
-    case 'editUnit':
-      // TODO
-      break;
-
     case 'delete':
       await deleteElement(mealID);
       return null;
@@ -54,7 +50,7 @@ function Meal({ meal }){
   // Set view to normal if any of the listed properties changed
   useEffect(() => {
     setView('normal');
-  }, [meal.calories, meal.tfat, meal.sfat, meal.carbs, meal.sugar, meal.protein]);
+  }, [meal.calories, meal.tfat, meal.sfat, meal.carbs, meal.sugar, meal.protein, meal.unit]);
 
   async function handleAdd(){
     const newMeal = {...meal};
@@ -72,8 +68,10 @@ function Meal({ meal }){
             <img src={image}></img> 
             <span className={styles.mealLabel}>{meal.label}</span>
           </div>
-          <button className={styles.addButton} onClick={handleAdd}>Add to my Today-list</button>
-          <div className={styles.buttonContainer}>
+          <div className={styles.upperButtonContainer}>
+            <button className={styles.addButton} onClick={handleAdd}>Add to my Today-list</button>
+          </div>
+          <div className={styles.lowerButtonContainer}>
             <button className={styles.infoButton} onClick={() => setView("info")}>Info</button>
             <button className={styles.editButton} onClick={() => setView("edit")}>Edit</button>
           </div>
@@ -85,9 +83,12 @@ function Meal({ meal }){
           <div className={styles.header}>
             <table className={`${styles.macroTable} ${styles.infoView}`}>
               <thead>
-                <tr>
-                  <th>Nutrition facts</th>
-                  <th>Per unit ({meal.unit})</th>
+                <tr className={styles.theadFirstRow}>
+                  <th colSpan={2}>Nutrition facts</th>
+                </tr>
+                <tr className={styles.theadSecondRow}>
+                  <th>Base unit</th>
+                  <th>{meal.unit}</th>
                 </tr>
               </thead>
               <tbody>
@@ -103,83 +104,85 @@ function Meal({ meal }){
             </table>
             <span className={styles.mealLabel}>{meal.label}</span>
           </div>
-          <button className={styles.addButton}>Add to my Today-list</button>
-          <div className={styles.buttonContainer}>
+          <div className={styles.upperButtonContainer}> 
+            <button className={styles.addButton}>Add to my Today-list</button>
+          </div>
+          <div className={styles.lowerButtonContainer}>
             <button className={styles.backButton} onClick={() => setView("normal")}>Back</button>
             <button className={styles.editButton} onClick={() => setView("edit")}>Edit</button>
           </div>
         </div>
       );
-
     case "edit":
       return (
-        <div className={styles.meal}>
-          <Form method='post'>
-            <div className={styles.header}>
-              <table className={`${styles.macroTable} ${styles.editView}`}>
-                <thead>
-                  <tr>
-                    <th>Nutrition facts</th>
-                    <th>
-                      Per unit <span className={styles.unitSize}>({meal.unit})</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {macros.map((keyName, i) => 
-                  <TRow
-                    key={i} 
-                    ind={i}
-                    view={view} 
-                    amount={meal[keyName]}>
-                  </TRow>
-                  )}
-                </tbody>
-              </table>
-            </div>
+        <Form method='post' className={styles.meal}>
+          <div className={styles.header}>
+            <table className={`${styles.macroTable} ${styles.editView}`}>
+             <thead>
+                <tr className={styles.theadFirstRow}>
+                  <th colSpan={2}>Nutrition facts</th>
+                </tr>
+                <tr className={styles.theadSecondRow}>
+                  <th>Base unit</th>
+                  <th>
+                    <input 
+                      name='unit'
+                      className={`${styles.editViewInput} ${styles.unitInput}`}
+                      type="text" 
+                      maxLength={15}
+                      placeholder={meal.unit}
+                    />
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {macros.map((keyName, i) => 
+                <TRow
+                  key={i} 
+                  ind={i}
+                  view={view} 
+                  amount={meal[keyName]}>
+                </TRow>
+                )}
+              </tbody>
+            </table>
+            <span className={styles.mealLabel}>{meal.label}</span>
+          </div>
 
-            <div className={styles.buttonContainer}>
-              <button 
-                name='button' 
-                value={['editImage', meal.id]}
-                className={styles.editImageButton}
-              >
-                Edit image
-              </button>
-              <button 
-                name='button' 
-                value={['editUnit', meal.id]} 
-                className={styles.editUnitButton}
-              >
-                Edit unit
-              </button>
-            </div>
-
-            <div className={styles.buttonContainer}>
-              <button 
-                type='submit'
-                name='button' 
-                value={['confirm', meal.id]} 
-                className={styles.confirmButton}
-              >
-                Confirm
-              </button>
-              <button 
-                className={styles.cancelButton} 
-                onClick={() => setView("normal")}
-              >
-                Cancel
-              </button>
-            </div>
+          <div className={styles.upperButtonContainer}>
+            <button 
+              name='button' 
+              value={['editImage', meal.id]}
+              className={styles.editImageButton}
+            >
+              Edit image
+            </button>
             <button 
               name='button' 
               value={['delete', meal.id]} 
               className={styles.deleteButton}
             >
-              Delete from my Library
+              Delete
             </button>
-          </Form>
-        </div>
+          </div>
+
+          <div className={styles.lowerButtonContainer}>
+            <button 
+              type='submit'
+              name='button' 
+              value={['confirm', meal.id]} 
+              className={styles.confirmButton}
+            >
+              Confirm
+            </button>
+            <button 
+              className={styles.cancelButton} 
+              onClick={() => setView("normal")}
+            >
+              Cancel
+            </button>
+          </div>
+        </Form>
       );
 
     default:
@@ -205,6 +208,7 @@ function TRow(props){
             name={macros[props.ind]}
             className={styles.editViewInput}
             type="text" 
+            maxLength={5}
             placeholder={props.amount + (props.ind === 0 ? " kcal" : "g")}/>
         </td>
       </tr>
